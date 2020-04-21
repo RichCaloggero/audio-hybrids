6f807fdcb22f7003ea5d019497c311349b776271
Rich <rjc@mit.edu>
2020-04-21 15:02:57 -0400

    * panner.js how accepts polar coordinates
    - these result in motion in the X-Z plane
    - how to do spherical polar coordinates??

--- panner.js
+++ panner.js
@@ -4,7 +4,9 @@ import * as element from "./element.js";
 import * as ui from "./ui.js";
 
 const defaults = {
-panningModel: {default: "HRTF"}
+panningModel: {default: "HRTF"},
+a: {default: 0, step: 0.05},
+r: {default: 0, min: 0, step: 1}
 };
 
 const Panner = element.create("panner", defaults, "createPanner", [
@@ -19,23 +21,43 @@ const Panner = element.create("panner", defaults, "createPanner", [
 "panningModel", "distanceModel", "maxDistance", "refDistance", "rolloffFactor",
 ], {
 
+r: {
+connect: element.connect,
+observe: (host, value) => {
+host.x = value * Math.cos(host.a);
+host.z = value * Math.sin(host.a);
+} // observe
+}, // r
 
-render: ({ bypass, mix, label,
-_depth, x,y,z,
+a: {
+connect: element.connect,
+observe: (host, value) => {
+host.x = host.r * Math.cos(value);
+host.z = host.r * Math.sin(value);
+} // observe
+}, // r
+
+
+render: ({ bypass, mix, label, _depth,
+x,y,z,
 orientationX, orientationY, orientationZ,
 distanceModel, maxDistance, refDistance, rolloffFactor,
 panningModel, innerAngle, outerAngle, outerGain,
+r, a
 }) => {
 	return html`
 <fieldset class="panner">
 ${ui.legend({ label, _depth })}
 ${ui.commonControls({ bypass, mix, defaults })}
 
-${ui.number("x", "x", x, defaults)}
+<br>${ui.number("x", "x", x, defaults)}
 ${ui.number("y", "y", y, defaults)}
 ${ui.number("z", "z", z, defaults)}
 
-${ui.number("orientationX", "orientationX", orientationX, defaults)}
+<br>${ui.number("r", "r", r, defaults)}
+${ui.number("a", "a", a, defaults)}
+
+<br>${ui.number("orientationX", "orientationX", orientationX, defaults)}
 ${ui.number("orientationY", "orientationY", orientationY, defaults)}
 ${ui.number("orientationZ", "orientationZ", orientationZ, defaults)}
 
