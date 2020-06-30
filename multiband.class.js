30c10547f61afbb3a82229421abade501754ca84
Rich <rjc@mit.edu>
2020-06-30 17:17:02 -0400

    * updated Multiband.class.js

--- multiband.class.js
+++ multiband.class.js
@@ -1,9 +1,11 @@
 class Multiband {
 constructor (input, bandCount = 3, order = 1) {
 this.band = [];
+this.input = input;
 this.output = [];
-this.context = input.context;
-if (!this.context) {
+const context = input.context;
+
+if (!context) {
 throw new Error("Multiband: input must be an audio node which is currently participating in an audio context");
 } // if
 
@@ -11,17 +13,24 @@ for (let i=0; i < bandCount; i++) {
 if (i === 0) this.band[i] = _createBand("lowshelf");
 else if (i === bandCount-1) this.band[i] = _createBand("highshelf");
 else this.band[i] = _createBand("peaking");
+output[i] = this.band.output;
 } // for
 
-_createBand (type) {
-const band = {type: type};
+function _createBand (type) {
+const band = {type: type, output: context.createGain()};
 if (type === "lowshelf") {
 band.lp = createFilters(context, "lowpass", order);
+input.connect(band.lp.input);
+band.lp.output.connect(band.output);
 } else if (type === "highshelf") {
 band.hp = createFilters(context, "highpass", order);
+input.connect(band.hp.input);
+band.hp.output.connect(band.output);
 } else {
 band.lp = createFilters(context, "lowpass", order);
 band.hp = createFilters(context, "highpass", order);
+input.connect(band.hp.input);
+band.lp.output.connect(band.output);
 } // if
 
 band.setLow = function (frequency) {if (this.lp) this.lp.set(frequency);};
@@ -32,9 +41,18 @@ return band;
 } // constructor
 
 setCutoffs (frequencies) {
-
+for (let i=0; i < bandCount; i++) {
+const frequency = frequencies[i];
+if (i === 0) {
+this.band[i].setLow(frequency);
+} else if (i === bandCount-1) {
+this.band[i].setLow(frequency);
+} else {
+this.band[i].setLow(frequency);
+this.band[i].setHigh(frequency);
+} // if
+} // for
 } // setCutoffs
-
 } // class Multiband
 
 
